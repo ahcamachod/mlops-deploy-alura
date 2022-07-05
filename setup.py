84df7c3bda417b25624b30a1fa70f2a3cc07b0b1
Alvaro Camacho <alvaro.camacho.diaz@gmail.com>
2022-07-05 04:08:02 -0300

    Primer commit del proyecto.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Estrategias para el deploy de modelos de machine learning de Alura Latam.',
+    author='alvaro.camacho',
+    license='',
+)
---
+++ src/app/main.py
@@ -0,0 +1,39 @@
+from flask import Flask,request,jsonify
+from flask_basicauth import BasicAuth
+import os
+import pandas as pd
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from textblob import TextBlob
+
+modelo = pickle.load(open('../../models/modelo.pkl','rb'))
+columnas = ['area','modelo','estacionamiento']
+
+app = Flask(__name__)
+app.config['BASIC_AUTH_USERNAME'] = os.environ.get('BASIC_AUTH_USERNAME')
+app.config['BASIC_AUTH_PASSWORD'] = os.environ.get('BASIC_AUTH_PASSWORD')
+basic_auth = BasicAuth(app)
+
+@app.route('/')
+
+def home():
+    return 'Esta es mi primera API.'
+
+@app.route('/sentimiento/<frase>')
+@basic_auth.required
+def sentimiento(frase):
+    tb = TextBlob(frase)
+    tb_en = tb.translate(from_lang='es', to='en')
+    polaridad = tb_en.sentiment.polarity
+    return f'La polaridad de la frase es: {polaridad}'
+
+@app.route('/precio_casas/', methods=['POST'])
+@basic_auth.required
+def precio_casas():
+    datos= request.get_json()
+    datos_input = [datos[col] for col in columnas]
+    precio = modelo.predict([datos_input])
+    return jsonify(precio=precio[0])
+
+app.run(debug=True, host='0.0.0.0')
